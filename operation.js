c30ea82d14333975b26051430f46afd84301c041
Hemen Girma <hemengirwerk@gmail.com>
2025-04-13 22:29:48 +0300

    added new file

--- operation.js
+++ operation.js
@@ -1,56 +1,60 @@
-const display=document.getElementById("display");
+const display = document.getElementById("display");
 let hasCalculated = false;
-function appendToDisplay(input){
-  
-  if(hasCalculated){
-    clearAll();
-    hasCalculated=false;
-  }
-  display.value+=input;
-}
-function clearLastCharacter(){
 
+function appendToDisplay(input) {
+  if (hasCalculated) {
+    clearAll();
+    hasCalculated = false;
+  }
+  display.value += input;
+}
+
+function clearLastCharacter() {
   display.value = display.value.slice(0, -1);
 }
+
 function calculate() {
   let expression = display.value;
 
-  // Replace percentage expressions
+
   expression = expression.replace(/(\d+)%/g, '($1 / 100)');
 
   let match;
 
-  // Process factorials in the expression
   while ((match = expression.match(/(\d+)!/))) {
     const number = parseInt(match[1], 10);
 
-    // Check if the number is a valid positive integer
-    if (isNaN(number) || number < 0 || !Number.isInteger(number)) {
+   
+    if (isNaN(number) || number < 0) {
       display.value = "Error"; 
       return;
     }
 
-    // Calculate factorial
+
     const factorialResult = factorial(number);
     expression = expression.replace(/(\d+)!/, factorialResult);
   }
 
   try {
-    // Evaluate the final expression after replacing factorials
+ 
     display.value = eval(expression);
-    hasCalculated = true; // Flag indicating a calculation has occurred
+    hasCalculated = true; 
   } catch (error) {
-    // Handle any errors during evaluation
+
     display.value = "Error";
   }
 }
 
 function factorial(n) {
   if (n < 0) {
-    return "Error"; // Factorial not defined for negative numbers
+    return "Error"; 
   }
   if (n === 0 || n === 1) {
     return 1;
   }
   return n * factorial(n - 1);
+}
+
+function clearAll() {
+  display.value = "";
 }
